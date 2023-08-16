e1091412e9e0430530366f403babbbbb9af6ee73
TheFastestMan <115644047+TheFastestMan@users.noreply.github.com>
2023-08-17 00:25:22 +0600

    All 3 methods are completed!

--- src/main/java/ConnectionRunner.java
+++ src/main/java/ConnectionRunner.java
@@ -1,11 +1,34 @@
 import util.ConnectionManager;
 
 import java.sql.SQLException;
+import java.sql.Savepoint;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class ConnectionRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Long id = 1L;
+        String seat = "A1";
+        Long coast = 210L;
+        String passengerName = "Антон Чехов";
+        String passportNo = "SB666D";
+        String flightNo = "MN6666";
+        String status = "DENIED666";
+        LocalDate departureDate = LocalDate.of(2020, 01, 22);
+        LocalDate arrivalDate = LocalDate.of(2020, 01, 22);
+
+
+        updateFlightAndTicketData(id,
+                seat, coast, passengerName, passportNo,
+                flightNo, status, departureDate.atStartOfDay(), arrivalDate.atStartOfDay());
 
         getFrequentNames();
+
+        getAmountOfTicketsPurchase();
+
+        updateTicket(seat, coast, passengerName, passportNo, id);
+
     }
 
     public static void getFrequentNames() {
@@ -34,19 +57,84 @@ public class ConnectionRunner {
         }
     }
 
+    public static void getAmountOfTicketsPurchase() {
+
+        String sql = """
+                SELECT passenger_name, COUNT(*) AS purchase
+                FROM ticket
+                         INNER JOIN
+                     flight ON flight.id = ticket.flight_id
+                GROUP BY passenger_name
+                ORDER BY purchase DESC;
+                """;
+        try (var connection = ConnectionManager.open()) {
+            var prepareStatement = connection.prepareStatement(sql);
+            try (var result = prepareStatement.executeQuery()) {
+                while (result.next()) {
+                    System.out.println(result.getString("passenger_name"));
+                    System.out.println(result.getInt("purchase"));
+                    System.out.println("--------------");
+
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void updateTicket(String seat, Long cost, String passengerName, String passportNo, Long id) {
+        String sql = """
+                UPDATE ticket SET seat_no = ?, cost = ?, passenger_name = ?, passport_no = ? WHERE id = ?;
+                """;
+        try (var connection = ConnectionManager.open()) {
+            var prepareStatement = connection.prepareStatement(sql);
+
+            prepareStatement.setString(1, String.valueOf(seat));
+            prepareStatement.setLong(2, cost);
+            prepareStatement.setString(3, String.valueOf(passengerName));
+            prepareStatement.setString(4, String.valueOf(passportNo));
+            prepareStatement.setLong(5, id);
+
+            var result = prepareStatement.executeUpdate();
+            System.out.println("Person with id " + id + " is updated.");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void updateFlightAndTicketData(Long id,
+                                                 String seat, Long cost, String passengerName, String passportNo,
+                                                 String flightNo, String status, LocalDateTime departureDate,
+                                                 LocalDateTime arrivalDate) throws SQLException {
+
+        String sql = """
+                UPDATE ticket SET seat_no = ?, cost = ?, passenger_name = ?, passport_no = ? WHERE flight_id = ?;
+                UPDATE flight SET flight_no = ?, departure_date = ?, arrival_date = ?, status = ? WHERE id = ?;
+                """;
+        try (var connection = ConnectionManager.open()) {
+            try {
+                var prepareStatement = connection.prepareStatement(sql);
+                connection.setAutoCommit(false);
+
+                prepareStatement.setString(1, String.valueOf(seat));
+                prepareStatement.setLong(2, cost);
+                prepareStatement.setString(3, String.valueOf(passengerName));
+                prepareStatement.setString(4, String.valueOf(passportNo));
+                prepareStatement.setLong(5, id);
+
+                prepareStatement.setString(6, String.valueOf(flightNo));
+                prepareStatement.setTimestamp(7, Timestamp.valueOf((departureDate)));
+                prepareStatement.setTimestamp(8, Timestamp.valueOf((arrivalDate)));
+                prepareStatement.setString(9, String.valueOf(status));
+                prepareStatement.setLong(10, id);
+
+                prepareStatement.executeUpdate();
+                connection.commit();
+
+            } catch (SQLException e) {
+                System.out.println("An error occurred: " + e.getMessage());
+                connection.rollback();
+            }
+        }
+    }
 }
-/*
-*  Написать класс с методами:
-
-Возвращает имена встречающиеся чаще всего done!!!
-
-Возвращает имена пассажиров и сколько билетов пассажир купил за все время
-
-Обновляет данные в таблице ticket по id
-
-Обновляет данные по flight_id в таблицах flight и ticket в одной транзакции
-* (если происходит ошибка, то все операции должны откатиться)
-*
-*
-*
-* */
